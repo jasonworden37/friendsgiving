474cd3fd474762e29a501e18fa90c9ade6184b74
jasonworden37 <jasonworden37@gmail.com>
2023-11-24 14:42:21 -0500

    new file:   images/background.png 	new file:   images/backgroundOld.png 	new file:   images/backgroundOld2.png 	deleted:    images/snowflake.jpeg 	deleted:    images/snowflake.webp 	modified:   index.html 	modified:   snowfall.js 	modified:   style.css

--- snowfall.js
+++ snowfall.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("snowflakes-container").appendChild(snowflake);
     
         const startPositionX = Math.random() * window.innerWidth;
-        const startPositionY = Math.random() * (window.innerHeight * 0.5); // Adjust 0.5 to set the starting height as a percentage of the window height
+        const startPositionY = Math.random() * (window.innerHeight * 0.1); // Adjust 0.5 to set the starting height as a percentage of the window height
         const animationDuration = Math.random() * 5 + 5; // between 5 and 10 seconds
         const animationDelay = Math.random() * 5; // between 0 and 5 seconds
     
