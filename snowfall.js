3f704d7fc87a7b1f3ec49adc408679c2866e3b49
jasonworden37 <jasonworden37@gmail.com>
2023-11-23 21:57:33 -0500

    new file:   images/background_christmas.jpeg 	deleted:    images/background_pumpkins.jpg 	new file:   images/snowflake.jpeg 	new file:   images/snowflake.png 	new file:   images/snowflake.webp 	modified:   index.html 	new file:   snowfall.js 	modified:   style.css

---
+++ snowfall.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const numSnowflakes = 100;
+
+    for (let i = 0; i < numSnowflakes; i++) {
+        createSnowflake();
+    }
+
+    function createSnowflake() {
+        const snowflake = document.createElement("div");
+        snowflake.className = "snowflake";
+        document.getElementById("snowflakes-container").appendChild(snowflake);
+    
+        const startPositionX = Math.random() * window.innerWidth;
+        const startPositionY = Math.random() * (window.innerHeight * 0.5); // Adjust 0.5 to set the starting height as a percentage of the window height
+        const animationDuration = Math.random() * 5 + 5; // between 5 and 10 seconds
+        const animationDelay = Math.random() * 5; // between 0 and 5 seconds
+    
+        snowflake.style.left = startPositionX + "px";
+        snowflake.style.top = startPositionY + "px";
+        snowflake.style.animationDuration = animationDuration + "s";
+        snowflake.style.animationDelay = "-" + animationDelay + "s"; // negative delay to start animations at different times
+    }
+    
+});
